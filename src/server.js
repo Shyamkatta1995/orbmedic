202361978e41a54b9572c58078695044e4790c54
Katta Shyamsundar reddy <kattashyamsundarreddy@MacBook-Pro.local>
2020-06-18 15:35:46 -0400

    application name changes

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/avurn-ng'));
+app.use(express.static(__dirname + '/dist/orbmedic'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/avurn-ng/index.html'));
+res.sendFile(path.join(__dirname+'/dist/orbmedic/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
