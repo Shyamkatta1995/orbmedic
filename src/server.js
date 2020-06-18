6f86693095968013808b3f91fed92b21ddf64bc9
Katta Shyamsundar reddy <kattashyamsundarreddy@MacBook-Pro.local>
2020-06-18 15:25:03 -0400

    updates to deploy to heoku

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/orbmedic'));
+app.use(express.static(__dirname + '/dist/avurn-ng'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/orbmedic/index.html'));
+res.sendFile(path.join(__dirname+'/dist/avurn-ng/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
